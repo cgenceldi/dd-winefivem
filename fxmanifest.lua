f8d0de849376b27b4a49ea0569d4c0a50d4525c7
DeadLight <61916204+cgenceldi@users.noreply.github.com>
2021-08-24 16:58:14 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Uzun Toplama'
+
+version '1.0.4'
+
+server_scripts {
+	'@extendedmode/locale.lua',
+	'locales/en.lua',
+	'server/main.lua',
+	'config.lua'
+}
+
+client_scripts {
+	'@extendedmode/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+
+
